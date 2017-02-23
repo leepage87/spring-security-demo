551c50964197b574ed3d19067a0a6f80a507e92d
Lee Page <lee.page87@gmail.com>
2017-02-22 21:02:24 -0600

    takin' a break

--- ui/src/main/java/com/example/UiApplication.java
+++ ui/src/main/java/com/example/UiApplication.java
@@ -2,6 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class UiApplication {
@@ -9,4 +14,18 @@ public class UiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
 	}
+	
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.httpBasic().and().authorizeRequests()
+			.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
+			.anyRequest().authenticated();
+		}
+	}
+	
+	
+	
 }
--- ui/src/main/resources/static/js/hello.js
+++ ui/src/main/resources/static/js/hello.js
@@ -1,12 +1,65 @@
-angular.module('hello', [])
-	.controller('home', function($http){
-//		this.greeting = {
-//				id: '123456',
-//				content: 'hello world'
-//			}
+//import homeController from ./home.controller.js;
+
+angular.module('hello', ['ngRoute'])
+	.config(function($routeProvider, $httpProvider){
+		
+		$routeProvider.when('/', {
+			templateUrl: 'home.html',
+			controller: 'home',
+			controllerAs: 'controller'
+		}).when('/login', {
+			templateUrl: 'login.html',
+			controller: 'navigation',
+			controllerAs: 'controller'
+		}).otherwise('/');
+		
+		$httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
+		
+	}).controller('home', function($http){
 		
 		var self = this;
+		//self is an alias for this so the below callback can reference the controller
 		$http.get('/resource/').then(function(response){
 			self.greeting = response.data;
 		});
+	}).controller('navigation', function($rootScope, $http, $location){
+		var self = this;
+		
+		//btoa() encodes in base64
+		var authenticate = function(credentials, callback) {
+			var headers = credentials ? {
+					authorization: "Basic " + btoa(credentials.username + ":" + credentials.password)
+				} : {};
+				
+			$http.get('user', {headers}).then(function(response) {
+//				if (response.data.name){
+//					$rootScope.authenticated = true;
+//				} else {
+//					$rootScope.authenticated = false;
+//				}
+				
+//				shorthand it by coercing to boolean for the sake of un-readability 
+				$rootScope.authenticated = !!response.data.name;
+				
+				callback && callback();
+			}, function(){
+				$rootScope.authenticated = false;
+				callback && callback();
+			})
+		}
+		
+		authenticate();
+		
+		self.credentials = {};
+		self.login = function(){
+			authenticate(self.credentials, function(){
+				if ($rootScope.authenticated){
+					$location.path("/");
+					self.error = false;
+				} else {
+					$location.path("/login");
+					self.error = true;
+				}
+			});
+		};
 	});
